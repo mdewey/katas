81000368cebb6043490bf691302323ec9925a3c5
Mark Dewey <mark.t.dewey@gmail.com>
2018-03-02 08:42:11 -0500

    added js for partiy

---
+++ parity-outlier/javscript/index.js
@@ -0,0 +1,23 @@
+const findOutlier = (numbers) => {
+    const _data = numbers.reduce((_data, num) => {
+        if (num % 2 ==0){
+            _data.evenCount++
+            _data.lastEven = num;
+        } else {
+            _data.oddCount++
+            _data.lastOdd = num;
+        }
+        return _data;
+    }, {
+        oddCount :0, 
+        evenCount:0,
+        lastOdd:0,
+        lastEven:0,
+    });
+    return _data.oddCount === 1 ? _data.lastOdd : _data.lastEven;
+}
+
+
+
+console.log(findOutlier([1,2,1,1,1,1,1,1]))
+console.log(findOutlier([2,2,2,1,2,2,2,2,2]))

2cc52671358f5f4aaeb6472ec18edccedc6dbcb7
Mark Dewey <mark.t.dewey@gmail.com>
2018-03-07 13:15:36 -0500

    added bit counting

---
+++ bit-counting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace bit_counting
+{
+    class Program
+    {
+
+        public static int CountBits(int n)
+        {
+            var binary = Convert.ToString(n, 2);
+            var count = binary.Count(c => c == '1');
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

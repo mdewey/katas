54858456321622e5ac80b3b26aa10bcc31d3f900
Mark Dewey <mark.t.dewey@gmail.com>
2018-03-09 13:41:06 -0500

    added next prefect sq

---
+++ next-perfect-square/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace next_perfect_square
+{
+    class Program
+    {
+
+        public static long FindNextSquare(long num)
+        {
+            // your code here
+            var root = Math.Sqrt((double)num);
+            if (root == Math.Floor(root)){
+                return (long) Math.Pow(++root, 2);
+            } else {
+                return -1;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

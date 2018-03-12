9041cf5e8c27b7c0549e21a22ad0864e5ff07400
Mark Dewey <mark.t.dewey@gmail.com>
2018-03-12 08:27:37 -0400

    added trolls & vowels for c#

---
+++ non-vowels-trolls/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace non_vowels_trolls
+{
+    class Program
+    {
+        public static string Disemvowel(string str)
+        {
+            var _rv = String.Join("", str.Where(s => !"aeiouAEIOU".Contains(s)));
+            return _rv;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

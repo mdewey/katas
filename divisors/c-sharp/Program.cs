c156e85995869d782b78f6d34a4bdf4062c4461e
Mark Dewey <mark.t.dewey@gmail.com>
2018-03-28 08:53:17 -0400

    added a few things

---
+++ divisors/c-sharp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace c_sharp
+{
+    class Program
+    {
+
+        public static void display(IEnumerable<int> arr){
+            Console.WriteLine("----");
+            if (arr == null) return;
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public static int[] Divisors(int n)
+        {
+            var rv = Enumerable.Range(2, n/2).ToArray().Where(w => n % w == 0).ToArray();
+            return rv.Length == 0 ? null : rv;
+        }
+
+        public static int[] Divisors(int n)
+        {
+            var rv = new List<int>();
+            var top = n/2;
+            for (int i = 0; i < n; i++)
+            {
+                if (n % 1 == 0){
+                    rv.Add(i);
+                }
+            }
+            
+            return rv.Count() == 0 ? null : rv.ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+          display(Divisors(12));
+          display(Divisors(25));
+          display(Divisors(13));
+          display(Divisors(24));
+            
+        }
+    }
+}
---
+++ mult-10/javascript/index.js
@@ -0,0 +1,30 @@
+    const closestMultiple10 = num => {
+        const mod = num % 10;
+        let rv = num;
+        if (mod >= 5){
+        rv += (10 - mod)  
+        } else {
+        rv -= mod 
+        }
+        return rv;
+    };
+
+
+  const duckduck = (players, goose) => {
+    let _goose = goose;
+    while (_goose > players.length){
+        _goose -= players.length;
+    }
+    return players[_goose-1].name;
+  }
+
+  class Player {
+    constructor(name) {
+        this.name = name;
+    }
+  }
+  
+  let ex_names = ["a", "b", "c", "d", "c", "e", "f", "g", "h", "z"];
+  let players = ex_names.map((n) => new Player(n));
+
+  console.log(duckduck(players, 12))

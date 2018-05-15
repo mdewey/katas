35e63f51b26c2dd29cf019c41b9124cc16776003
Mark Dewey <mark.t.dewey@gmail.com>
2018-05-15 15:17:10 -0400

    added count-dupes

---
+++ count-dupes/c-sharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace c_sharp
+{
+    class Program
+    {
+
+        public static int DuplicateCount(string str)
+        {
+            return str.ToLower().Distinct().ToDictionary(k => k, v => str.Count(c => v == c)).Count(w => w.Value >= 2);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

9a8739bc0c60461ef36420157972e3185e3448d1
Mark Dewey <mark.t.dewey@gmail.com>
2018-05-29 09:35:47 -0400

    added array diff

---
+++ arrayDiff/c-sharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace c_sharp
+{
+    class Program
+    {
+
+        public static int[] ArrayDiff(int[] a, int[] b)
+        {
+            return a.Where(w => !b.Contains(w)).ToArray();  
+         
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

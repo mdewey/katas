818bfd6da7b4bcf5aca0ca1e57b9700faddb275e
Mark Dewey <mark.t.dewey@gmail.com>
2018-07-13 10:22:30 -0400

    added friday the 13th

---
+++ friday-13th/c-sharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace c_sharp
+{
+    class Program
+    {
+        public class Kata
+        {
+            public static IEnumerable<string>  KillCount(Dictionary<string, int> counselors, int jason)
+            {
+                // insert your code here
+                return counselors.Where(person => person.Value < jason).Select(s => s.Key);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> counselors = new Dictionary<string, int>
+            {
+                {"Mike", 7},
+                {"Alysa", 3}
+            };
+            Console.WriteLine(Kata.KillCount(counselors, 7));
+        }
+    }
+}
